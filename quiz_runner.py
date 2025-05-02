9ddb115aba0ca110531bee3d0b46707eeaa2d1df
unknown <lawrencekulet@gmail.com>
2025-05-02 23:02:14 +0800

    pseudocode for quiz runner pt.1

---
+++ quiz_runner.py
@@ -0,0 +1,14 @@
+# Quiz Runner
+
+# pseudocode for quiz runner
+
+# initialize the main window and the user interface elements
+# make the title of the program "QuizRunner"
+
+# create an input for users to enter their name
+# display a label "Enter your name:"
+# display an entry box for the user to input their name
+
+# create start quiz button
+# display a button to start quiz
+ 
